e6df25d67d733f7ee70d8bd3e7a547c833237959
mantlebee <mantlebee@outlook.com>
2022-06-05 07:59:46 +0200

    Publish

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,16 @@
-import typescript from '@rollup/plugin-typescript';
+import cleaner from "rollup-plugin-cleaner";
+import typescript from "@rollup/plugin-typescript";
+
+const outDir = "dist"
 
 export default {
-    input: 'src/index.ts',
+    input: "src/index.ts",
     output: {
-        dir: 'dist',
-        format: 'es'
+        dir: outDir,
+        format: "cjs",
     },
-    plugins: [typescript()]
+    plugins: [
+        cleaner({ targets: [outDir] }),
+        typescript()
+    ]
 };
