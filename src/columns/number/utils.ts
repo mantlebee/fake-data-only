68356d2772b45056364c1bd477183ec7d6953154
mantlebee <mantlebee@outlook.com>
2022-06-05 09:26:26 +0200

    Updated ts-core

--- src/columns/number/utils.ts
+++ src/columns/number/utils.ts
@@ -6,6 +6,5 @@ export function FdoColumnNumberValueDelegate(
   options: FdoColumnNumberOptions
 ): number {
   const { decimals = 0, max, min = 0 } = options;
-  if (max === min) return max;
   return generateRandomNumber(max, min, decimals);
 }
