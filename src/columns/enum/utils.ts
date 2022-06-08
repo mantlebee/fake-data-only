5eb45dc7aaf9c1c402b9be05de94bf6b5cef8995
mantlebee <mantlebee@outlook.com>
2022-06-08 07:53:16 +0200

    Publish

--- src/columns/enum/utils.ts
+++ src/columns/enum/utils.ts
@@ -1,4 +1,4 @@
-import { extractRandomItem, generateRandomNumber } from "@mantlebee/ts-core";
+import { extractRandomItem } from "@mantlebee/ts-core";
 import { FdoColumnEnumOptions } from "./types";
 
 export function FdoColumnEnumValueDelegate<TEnum>(
