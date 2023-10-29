4309f63a6ea66f6150904d1fb8d3a164b56fde22
mantlebee <mantlebee@outlook.com>
2023-10-29 11:34:49 +0100

    Removed unused code

--- src/columns/date/__tests__/utils.test.ts
+++ src/columns/date/__tests__/utils.test.ts
@@ -1,15 +1,5 @@
 import { getColumnDateValue } from "../utils";
 
-type RowTest = {
-  dateFrom: Date;
-  dateTo: Date;
-};
-
-const row: RowTest = {
-  dateFrom: new Date(2022, 4, 3, 0, 0, 0),
-  dateTo: new Date(2022, 4, 3, 23, 59, 59),
-};
-
 const firstDate = new Date(1970, 0, 1, 0, 0, 0);
 
 describe("ColumnDate", () => {
