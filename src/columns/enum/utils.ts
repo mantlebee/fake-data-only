58af9519247a4600575033123d5d782e132f8d10
mantlebee <mantlebee@outlook.com>
2024-09-06 21:10:16 +0200

    Updated packages

--- src/columns/enum/utils.ts
+++ src/columns/enum/utils.ts
@@ -1,7 +1,7 @@
 import { extractRandomItem } from "@mantlebee/ts-random";
 
 export function getColumnEnumValue<TEnum>(enumerative: TEnum): TEnum {
-  const keysAndValues = Object.keys(enumerative);
+  const keysAndValues = Object.keys(Object(enumerative));
   const keys = keysAndValues.splice(keysAndValues.length / 2);
   const randomKey = extractRandomItem(keys);
   return enumerative[randomKey as keyof TEnum] as unknown as TEnum;
