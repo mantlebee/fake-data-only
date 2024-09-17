a6e790927b90d696d4630cba8ff2abe6040044e1
mantlebee <mantlebee@outlook.com>
2024-09-17 20:19:56 +0200

    Optimized a getProbabilityColumnValue test

--- src/columns/probability/__tests__/utils.test.ts
+++ src/columns/probability/__tests__/utils.test.ts
@@ -1,4 +1,3 @@
-import { List } from "@mantlebee/ts-core";
 import { getProbabilityColumnValue } from "../utils";
 
 describe("ProbabilityColumn", () => {
@@ -7,17 +6,17 @@ describe("ProbabilityColumn", () => {
       it("Extract a random value from a probability values map. More a value has a higher probability, more is possible it is extracted from the map.", () => {
         const falseProbability = 20;
         const trueProbability = 80;
-        const falseValues: List<boolean> = [];
-        const trueValues: List<boolean> = [];
+        let falseCount = 0;
+        let trueCount = 0;
         for (let i = 0; i < 100; i++) {
           const value = getProbabilityColumnValue({
             values: { [trueProbability]: true, [falseProbability]: false },
           });
-          if (value) trueValues.push(value);
-          else falseValues.push(value);
+          if (value) trueCount++;
+          else falseCount++;
         }
-        expect(trueValues.length).toBeGreaterThan(falseProbability);
-        expect(falseValues.length).toBeLessThan(trueProbability);
+        expect(trueCount).toBeGreaterThanOrEqual(falseProbability);
+        expect(falseCount).toBeLessThanOrEqual(trueProbability);
       });
       it("If none of the values has been extracted because of unluck probability, the most probable is returned", () => {
         const value = getProbabilityColumnValue({
